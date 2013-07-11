3c4afef58af1d962ef7ad436aea0724a6fad277a
Ubuntu <ubuntu@ip-172-31-30-229.us-west-2.compute.internal>
2013-07-11 05:02:44 +0000

    Adjusted web.js to read 'index.html' second time

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var fs = require('fs');
 
-var return = fs.readFileSync('index.html');
+var buffer = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(return.toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
